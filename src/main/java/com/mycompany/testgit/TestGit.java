067d0ed15e2d18e4359ba8158da4e0ec88699f11
14257112 <14257112@205-24.Fortis.local>
2026-05-28 08:44:10 +0200

    Beispiel

---
+++ src/main/java/com/mycompany/testgit/TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgit;
+
+/**
+ *
+ * @author 14257112
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
