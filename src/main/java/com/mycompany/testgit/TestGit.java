9799d3ec0d462bdeda3f572b1318cde5e38aaae7
HodiModi <jakob.weissbach@gmail.com>
2026-06-11 11:43:35 +0200

    Update TestGit.java

--- src/main/java/com/mycompany/testgit/TestGit.java
+++ src/main/java/com/mycompany/testgit/TestGit.java
@@ -8,9 +8,77 @@ package com.mycompany.testgit;
  *
  * @author 14257112
  */
-public class TestGit {
+
+import java.util.Scanner;
+
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Zoo Ticket System ===");
+
+        System.out.print("Anzahl Erwachsene: ");
+        int erwachsene = scanner.nextInt();
+
+        System.out.print("Anzahl Kinder (6-17 Jahre): ");
+        int kinder = scanner.nextInt();
+
+        System.out.print("Anzahl Kleinkinder (unter 6 Jahre): ");
+        int kleinkinder = scanner.nextInt();
+
+        System.out.print("Parkplatz benötigt? (j/n): ");
+        char parken = scanner.next().toLowerCase().charAt(0);
+
+        boolean parkplatz = parken == 'j';
+
+        System.out.print("Ziegenfütterung buchen? (j/n): ");
+        boolean ziegen = scanner.next().toLowerCase().charAt(0) == 'j';
+
+        System.out.print("Orca-Show buchen? (j/n): ");
+        boolean orca = scanner.next().toLowerCase().charAt(0) == 'j';
+
+        Ticket ticket = new Ticket(
+                erwachsene,
+                kinder,
+                kleinkinder,
+                parkplatz,
+                ziegen,
+                orca
+        );
+
+        Auftrag auftrag = new Auftrag();
+
+        System.out.println("\n===== RECHNUNG =====");
+
+        System.out.println("Erwachsene: " + erwachsene + " x 25€");
+        System.out.println("Kinder: " + kinder + " x 15€");
+        System.out.println("Kleinkinder: " + kleinkinder + " x 0€");
+
+        if (parkplatz)
+            System.out.println("Parkplatz: 5€");
+
+        if (ziegen)
+            System.out.println("Ziegenfütterung: 3€");
+
+        if (orca)
+            System.out.println("Orca-Show: 8€");
+
+        System.out.println("---------------------");
+        System.out.println("Gesamtpreis: " + ticket.berechnePreis() + "€");
+        System.out.println();
+
+        System.out.println("Auftragsnummer:");
+        System.out.println(auftrag.getAuftragsnummer());
+
+        System.out.println("\nBarcode:");
+        System.out.println(
+                BarcodeGenerator.erzeugeBarcode(
+                        auftrag.getAuftragsnummer()
+                )
+        );
+
+        scanner.close();
     }
 }
